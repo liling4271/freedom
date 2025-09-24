bf2b079ece2f4085ca8cbd65fdf7599a53043627
liling <liling4271@gmail.com>
2025-09-24 18:21:13 +0800

    添加gin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,43 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
+// 响应格式
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
 func main() {
-	_ = os.Setenv("DEBUG", "true")
+	r := gin.Default()
+
+	r.GET("/health", healthCheck)
 	fmt.Println("启动！")
+
+	// API v1 路由组
+	// v1 := r.Group("/api/v1")
+	{
+
+	}
+
+	log.Println("Freedom 服务启动，端口号：8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Println("服务启动失败")
+	}
+}
+
+// 健康检查
+func healthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, Response{
+		Code:    200,
+		Message: "服务正常运行",
+		Data:    gin.H{"timestamp": time.Now().Unix()},
+	})
 }
